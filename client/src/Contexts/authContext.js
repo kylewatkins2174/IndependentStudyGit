646a0bf29be136f58e02f7c47bb44a287361bd77
haydenmarshall65 <hem035@shsu.edu>
2023-04-26 13:34:43 -0500

    Added comments and deleted unused code

--- client/src/Contexts/authContext.js
+++ client/src/Contexts/authContext.js
@@ -1,22 +1,24 @@
 import {createContext, useState} from "react";
 
-export const AuthContext = createContext();
-
-export const AuthContextProvider = ({children}) => {
-
-    const [userValues, setUserValues] = useState({
-        userName: 'RodneyReedrocks123',
-        authorized: true,
-        isAdmin: true,
-        depId: 1,
-    });
-
-    const updateUser = ({user}) => {
-        setUserValues(user);
-    }
-
-
-    return(
+export const AuthContext = createContext();                 // A new Context is created, labeled AuthContext. This context is meant
+                                                            // to hold the user values, such as their username, authorization status,
+export const AuthContextProvider = ({children}) => {        // admin status, and department.
+                                                            //
+    const [userValues, setUserValues] = useState({          // The Context state object variable, userValues, is declared here. It can
+        userName: 'RodneyReedrocks123',                     // be used on any page, using the syntax "userValues.userName". 
+        authorized: true,                                   //
+        isAdmin: true,                                      //
+        depId: 1,                                           //
+    });                                                     //
+                                                            //
+    const updateUser = ({user}) => {                        // updateUser() is a function used to set the context variable to a new user.
+        setUserValues(user);                                // It takes in the user values from the login, and then uses setUserValues()
+    }                                                       // to update it.
+                                                            //
+                                                            //
+    return(                                                 // This is the AuthContextProvider to be wrapped around the App in index.js
+                                                            // so that every page will hold the variables necessary. The "value" declares
+                                                            // what is shared for other components to access.
         <AuthContext.Provider value={{userValues, updateUser}}>
             {children}
         </AuthContext.Provider>
