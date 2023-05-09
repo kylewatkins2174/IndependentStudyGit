1bde2c86832a18b73e03b9c2022a058a64b3a198
haydenmarshall65 <hem035@shsu.edu>
2023-05-09 15:12:40 -0500

    changes

--- client/src/Contexts/authContext.js
+++ client/src/Contexts/authContext.js
@@ -1,33 +1,39 @@
 import {createContext, useState, useEffect} from "react";
 import requestServer from "../axios";
-import axios from "axios"
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
-    const [userValues, setUserValues] = useState(
-        JSON.parse(localStorage.getItem("user")) || null
-        );
+    const [userValues, setUserValues] = useState();    
 
-        const login = async (inputs) => {
-            const res = await requestServer.post("http://localhost:8800/api/auth/login", inputs, {
-                withCredentials: true
-            });
+    const login = async (inputs) => {
+        const res = await requestServer.post("http://localhost:8800/api/auth/login", inputs, {
+            withCredentials: true
+        });
+
+        console.log(res.data)
 
         setUserValues(res.data);
-
-        console.log(userValues);
     }
 
-
-    useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(userValues))
-    }, [userValues]);
+    const getUser = async() => {
+        try{
+            const res = await requestServer.post("http://localhost:8800/api/auth/userInfo")
+            
+            const user = {
+                "userId" : res.data.userId,
+                "username" : res.data.username,
+                "departmentId" : res.data.departmentId
+            }
+            setUserValues(user);
+        }catch(error){
+            console.log(error)
+        }
+    }
 
     return(
-        <AuthContext.Provider value={{userValues, login}}>
+        <AuthContext.Provider value={{userValues, getUser, login}}>
             {children}
         </AuthContext.Provider>
     )
-
 };
